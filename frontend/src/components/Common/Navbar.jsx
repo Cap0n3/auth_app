d11664e2ecdfbe1c1d872fe0a465cfdbb0395d58
Cap0n3 <cap0n3@protonmail.com>
2024-03-01 20:00:58 +0100

    Added profile avatar in navbar

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -3,18 +3,24 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { useContext, useEffect } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../../routes/root';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../services/userservice';
 
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Nav = () => {
     const { client, currentUser, setCurrentUser, isAuthenticated, setIsAuthenticated } = useContext(UserContext);
     const navigate = useNavigate();
+    const [anchorElUser, setAnchorElUser] = useState(null);
 
-    const handleLogout = async (e) => {
-        e.preventDefault();
+    const handleLogout = async () => {
         try {
             await logout();
             setIsAuthenticated(false);
@@ -32,6 +38,31 @@ const Nav = () => {
         }
     }, [isAuthenticated]);
 
+    // === USER MENU === //
+    
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleMenuClick = (setting) => {
+        console.log('Setting clicked:', setting);
+
+        if (setting === 'Dashboard') {
+            navigate('/dashboard');
+        }
+
+        if (setting === 'Logout') {
+            handleLogout();
+        }
+
+        handleCloseUserMenu();
+    }
+    
+    
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -42,8 +73,40 @@ const Nav = () => {
                     <Typography variant="span" component="div" mr={3}>
                         {isAuthenticated && currentUser ? `Welcome, ${currentUser.username}` : ''}
                     </Typography>
+                    
+                    {/* === PROFILE AVATAR OR SIGN IN === */}
                     {isAuthenticated ?
-                        <Button variant="contained" onClick={e => handleLogout(e)}>Log out</Button> :
+                        // If user is authenticated, show the user avatar
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <Avatar alt="Profile Pic" src="https://picsum.photos/200" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={() => handleMenuClick(setting)}>
+                                        <Typography textAlign="center">{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box> :
+                        // If user is not authenticated, show the sign in button
                         <Link to="/signin">
                             <Button variant="contained">Sign In</Button>
                         </Link>
